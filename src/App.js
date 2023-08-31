e8d2fc75c8f3fc93908c07c97cbaf5f95299d292
Bruce Bermel <brucebermel14@gmail.com>
2023-08-31 16:33:21 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import forte from "./images/forte.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={forte} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
